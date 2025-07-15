96aebe4cf7b14f18347bf3fd1cb40444940f15a2
Meloncaully <maloneingham@gmail.com>
2025-07-14 23:05:19 -0400

    organized Workspace

---
+++ handler_user.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/MelonCaully/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) <= 0 {
+		return fmt.Errorf("no command present")
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.Cfg.SetUser(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User has been set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("no username provided")
+	}
+
+	name := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      name,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	err = s.Cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User created successfully.")
+	printUser(user)
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
+}
--- main.go
+++ main.go
@@ -1,97 +1,19 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/MelonCaully/gator/internal/config"
 	"github.com/MelonCaully/gator/internal/database"
-	"github.com/google/uuid"
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	db  *database.Queries
 	Cfg *config.Config
-}
-
-type commands struct {
-	Command map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	handler, ok := c.Command[cmd.Name]
-	if !ok {
-		return fmt.Errorf("unknown command: %s", cmd.Name)
-	}
-	return handler(s, cmd)
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	if c.Command == nil {
-		c.Command = make(map[string]func(*state, command) error)
-	}
-	c.Command[name] = f
-}
-
-type command struct {
-	Name string
-	Args []string
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.Args) <= 0 {
-		return fmt.Errorf("no command present")
-	}
-	if _, err := s.db.GetUser(context.Background(), cmd.Args[0]); err != nil {
-		if err == sql.ErrNoRows {
-			fmt.Fprintf(os.Stderr, "User '%s' does not exist.\n", cmd.Args[0])
-			os.Exit(1)
-		}
-		return fmt.Errorf("failed to check user existence: %w", err)
-	}
-	if err := s.Cfg.SetUser(cmd.Args[0]); err != nil {
-		return fmt.Errorf("failed to save user: %w", err)
-	}
-	fmt.Println("User has been set")
-	return nil
-}
-
-func handlerRegister(s *state, cmd command) error {
-	if len(cmd.Args) < 1 {
-		return fmt.Errorf("no username provided")
-	}
-
-	id := uuid.New()
-	name := cmd.Args[0]
-	now := time.Now()
-
-	_, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
-		ID:        id,
-		Name:      name,
-		CreatedAt: now,
-		UpdatedAt: now,
-	})
-
-	if err != nil {
-		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
-			fmt.Fprintf(os.Stderr, "User with name '%s' already exists.\n", name)
-			os.Exit(1)
-		}
-		return fmt.Errorf("failed to create user: %w", err)
-	}
-
-	if err := s.Cfg.SetUser(cmd.Args[0]); err != nil {
-		return fmt.Errorf("failed to set user in config: %w", err)
-	}
-
-	fmt.Println("User created successfully.")
-	log.Printf("Created user: ID=%s, Name=%s, CreatedAt=%s", id.String(), name, now.Format(time.RFC3339))
-	return nil
 }
 
 func main() {
@@ -107,7 +29,7 @@ func main() {
 		Cfg: &cfg,
 	}
 	c := commands{
-		Command: make(map[string]func(*state, command) error),
+		Commands: make(map[string]func(*state, command) error),
 	}
 
 	// Establish a connection with the database
@@ -128,6 +50,7 @@ func main() {
 	// register commands
 	c.register("login", handlerLogin)
 	c.register("register", handlerRegister)
+	c.register("reset", handlerReset)
 
 	// Initialize the command struct
 	cmd := command{
